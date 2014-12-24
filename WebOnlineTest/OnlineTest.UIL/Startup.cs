8970a9c752b1b65d993296bc659d15bdb91498d8
vijayjha <mailtovijayjha@gmail.com>
2014-12-24 13:39:49 +0530

    Very firs commit after adding a new projectsto repository

---
+++ WebOnlineTest/OnlineTest.UIL/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineTest.UIL.Startup))]
+namespace OnlineTest.UIL
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
