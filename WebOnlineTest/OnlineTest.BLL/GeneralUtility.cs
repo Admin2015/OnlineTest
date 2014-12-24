f23ca3e5890f5cab5abd0d28c3a1d72306af1adb
vijayjha <mailtovijayjha@gmail.com>
2014-12-24 14:06:58 +0530

    Added a BLL project

---
+++ WebOnlineTest/OnlineTest.BLL/GeneralUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineTest.BLL
+{
+    public class GeneralUtility
+    {
+        public static bool DateCompare(DateTime d1, DateTime d2)
+        {
+            return d1 >= d2;
+        }
+    }
+    
+}
